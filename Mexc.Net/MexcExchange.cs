891e33ac6bf49a2de0bccc1fde64f201564e40ec
JKorf <jankorf91@gmail.com>
2024-04-26 15:40:50 +0200

    wip

---
+++ Mexc.Net/MexcExchange.cs
@@ -0,0 +1,25 @@
+﻿namespace Mexc.Net
+{
+    /// <summary>
+    /// Mexc exchange information and configuration
+    /// </summary>
+    public static class MexcExchange
+    {
+        /// <summary>
+        /// Exchange name
+        /// </summary>
+        public static string ExchangeName => "Mexc";
+
+        /// <summary>
+        /// Url to the main website
+        /// </summary>
+        public static string Url { get; } = "https://www.mexc.com";
+
+        /// <summary>
+        /// Urls to the API documentation
+        /// </summary>
+        public static string[] ApiDocsUrl { get; } = new[] {
+            "https://mexcdevelop.github.io/apidocs/spot_v3_en/#introduction"
+            };
+    }
+}
