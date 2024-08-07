2a558792df9f246bbc9eeb4f7518bd5e94a241bb
Jan Korf <jankorf91@gmail.com>
2024-08-07 19:24:14 +0200

    CryptoExchange V7.11.0 (#11)
    Updated CryptoExchange.Net to V7.11.0
    Fixed deserialization error on GetTickers due to too large number

--- Mexc.Net/MexcAuthenticationProvider.cs
+++ Mexc.Net/MexcAuthenticationProvider.cs
@@ -10,8 +10,6 @@ namespace Mexc.Net
 {
     internal class MexcAuthenticationProvider : AuthenticationProvider
     {
-        public string GetApiKey() => _credentials.Key!.GetString();
-
         public MexcAuthenticationProvider(ApiCredentials credentials) : base(credentials)
         {
         }
@@ -20,26 +18,38 @@ namespace Mexc.Net
             RestApiClient apiClient,
             Uri uri,
             HttpMethod method,
-            IDictionary<string, object> uriParameters,
-            IDictionary<string, object> bodyParameters,
-            Dictionary<string, string> headers,
+            ref IDictionary<string, object>? uriParameters,
+            ref IDictionary<string, object>? bodyParameters,
+            ref Dictionary<string, string>? headers,
             bool auth,
             ArrayParametersSerialization arraySerialization,
             HttpMethodParameterPosition parameterPosition,
             RequestBodyFormat requestBodyFormat)
         {
-            headers.Add("X-MEXC-APIKEY", _credentials.Key!.GetString());
+            headers ??= new Dictionary<string, string>();
+            headers.Add("X-MEXC-APIKEY", _credentials.Key);
 
             if (!auth)
                 return;
 
-            var parameters = parameterPosition == HttpMethodParameterPosition.InUri ? uriParameters : bodyParameters;
+            IDictionary<string, object> parameters;
+            if (parameterPosition == HttpMethodParameterPosition.InUri)
+            {
+                uriParameters ??= new Dictionary<string, object>();
+                parameters = uriParameters;
+            }
+            else
+            {
+                bodyParameters ??= new Dictionary<string, object>();
+                parameters = bodyParameters;
+            }
             var timestamp = GetMillisecondTimestamp(apiClient);
             parameters.Add("timestamp", timestamp);
 
             if (_credentials.CredentialType == ApiCredentialsType.Hmac)
             {
-                uri = uri.SetParameters(uriParameters, arraySerialization);
+                if (uriParameters != null)
+                    uri = uri.SetParameters(uriParameters, arraySerialization);
                 parameters.Add("signature", SignHMACSHA256(parameterPosition == HttpMethodParameterPosition.InUri ? uri.Query.Replace("?", "") : parameters.ToFormData()));
             }
             else
