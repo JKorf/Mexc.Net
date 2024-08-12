af32556166a48d51f35db7981fcf3eea65b94dfa
JKorf <jankorf91@gmail.com>
2024-08-12 16:58:50 +0200

    wip

--- Mexc.Net/Clients/SpotApi/MexcSocketClientSpotApiShared.cs
+++ Mexc.Net/Clients/SpotApi/MexcSocketClientSpotApiShared.cs
@@ -1,7 +1,6 @@
 ﻿using Mexc.Net;
 using Mexc.Net.Interfaces.Clients.SpotApi;
 using CryptoExchange.Net.Objects;
-using CryptoExchange.Net.SharedApis.Interfaces;
 using CryptoExchange.Net.SharedApis.RequestModels;
 using CryptoExchange.Net.SharedApis.ResponseModels;
 using System;
@@ -12,6 +11,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using CryptoExchange.Net.SharedApis.Enums;
 using CryptoExchange.Net.Objects.Sockets;
+using CryptoExchange.Net.SharedApis.Models.Socket;
+using CryptoExchange.Net.SharedApis.Interfaces.Socket;
 using CryptoExchange.Net.SharedApis.SubscribeModels;
 
 namespace Mexc.Net.Clients.SpotApi
@@ -23,13 +24,23 @@ namespace Mexc.Net.Clients.SpotApi
         async Task<CallResult<UpdateSubscription>> ITickerSocketClient.SubscribeToTickerUpdatesAsync(TickerSubscribeRequest request, Action<DataEvent<SharedTicker>> handler, CancellationToken ct)
         {
             var symbol = FormatSymbol(request.BaseAsset, request.QuoteAsset, request.ApiType);
-            var result = await SubscribeToMiniTickerUpdatesAsync(symbol, update => handler(update.As(new SharedTicker
-            {
-                Symbol = update.Data.Symbol,
-                HighPrice = update.Data.HighPrice,
-                LastPrice = update.Data.LastPrice,
-                LowPrice = update.Data.LowPrice
-            })), ct: ct).ConfigureAwait(false);
+            var result = await SubscribeToMiniTickerUpdatesAsync(symbol, update => handler(update.As(new SharedTicker(symbol, update.Data.LastPrice, update.Data.HighPrice, update.Data.LowPrice)))).ConfigureAwait(false);
+
+            return result;
+        }
+
+        async Task<CallResult<UpdateSubscription>> ITradeSocketClient.SubscribeToTradeUpdatesAsync(TradeSubscribeRequest request, Action<DataEvent<IEnumerable<SharedTrade>>> handler, CancellationToken ct)
+        {
+            var symbol = FormatSymbol(request.BaseAsset, request.QuoteAsset, request.ApiType);
+            var result = await SubscribeToTradeUpdatesAsync(symbol, update => handler(update.As(update.Data.Select(x => new SharedTrade(x.Quantity, x.Price, x.Timestamp)))), ct).ConfigureAwait(false);
+
+            return result;
+        }
+
+        async Task<CallResult<UpdateSubscription>> IBookTickerSocketClient.SubscribeToBookTickerUpdatesAsync(BookTickerSubscribeRequest request, Action<DataEvent<SharedBookTicker>> handler, CancellationToken ct)
+        {
+            var symbol = FormatSymbol(request.BaseAsset, request.QuoteAsset, request.ApiType);
+            var result = await SubscribeToBookTickerUpdatesAsync(symbol, update => handler(update.As(new SharedBookTicker(update.Data.BestAskPrice, update.Data.BestAskQuantity, update.Data.BestBidPrice, update.Data.BestBidQuantity))), ct).ConfigureAwait(false);
 
             return result;
         }
