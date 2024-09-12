be900ef1a1b9e86445ff2bbdae488111be74ea43
JKorf <jankorf91@gmail.com>
2024-09-12 22:17:12 +0200

    wip

--- Mexc.Net/Clients/SpotApi/MexcRestClientSpotApiShared.cs
+++ Mexc.Net/Clients/SpotApi/MexcRestClientSpotApiShared.cs
@@ -182,10 +182,10 @@ namespace Mexc.Net.Clients.SpotApi
                 SharedTimeInForce.FillOrKill
             },
             new SharedQuantitySupport(
-                SharedQuantityType.BaseAssetQuantity,
-                SharedQuantityType.BaseAssetQuantity,
-                SharedQuantityType.Both,
-                SharedQuantityType.Both));
+                SharedQuantityType.BaseAsset,
+                SharedQuantityType.BaseAsset,
+                SharedQuantityType.BaseAndQuoteAsset,
+                SharedQuantityType.BaseAndQuoteAsset));
 
         async Task<ExchangeWebResult<SharedId>> ISpotOrderRestClient.PlaceSpotOrderAsync(PlaceSpotOrderRequest request, ExchangeParameters? exchangeParameters, CancellationToken ct)
         {
