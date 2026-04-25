6eb3d8bd7fc1e7849d69cb1176f967b4471bf8ab
nils2525 <nilslehb@gmail.com>
2026-04-25 15:20:36 +0200

    Added GetOfflineSymbols rest request

---
+++ Mexc.Net/Objects/Models/Spot/MexcOfflineSymbol.cs
@@ -0,0 +1,28 @@
+﻿using Mexc.Net.Enums;
+
+namespace Mexc.Net.Objects.Models.Spot
+{
+    /// <summary>
+    /// Offline symbol
+    /// </summary>
+    public record MexcOfflineSymbol
+    {
+        /// <summary>
+        /// ["<c>symbol</c>"] The symbol
+        /// </summary>
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; } = string.Empty;
+
+        /// <summary>
+        /// ["<c>status</c>"] Status of the symbol, paused or offline
+        /// </summary>
+        [JsonPropertyName("status")]
+        public SymbolStatus Status { get; set; }
+
+        /// <summary>
+        /// ["<c>offlineTime</c>"] The time when the symbol went offline
+        /// </summary>
+        [JsonPropertyName("offlineTime")]
+        public DateTime? OfflineTime { get; set; }
+    }
+}
