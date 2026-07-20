ce8df4382469e49eb97b9447fe375df194ee802e
Jkorf <jankorf91@gmail.com>
2026-07-20 14:25:41 +0200

    wip

--- Mexc.Net/Clients/SpotApi/MexcRestClientSpotApiShared.cs
+++ Mexc.Net/Clients/SpotApi/MexcRestClientSpotApiShared.cs
@@ -77,7 +77,7 @@ namespace Mexc.Net.Clients.SpotApi
         #endregion
 
         #region Spot Symbol client
-        SharedSymbolCatalog? ISpotSymbolRestClient.SpotSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_topicId, EnvironmentName, null);
+        SharedSymbolCatalog? ISpotSymbolRestClient.SpotSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicId, EnvironmentName, null);
         GetSpotSymbolsOptions ISpotSymbolRestClient.GetSpotSymbolsOptions { get; } = new GetSpotSymbolsOptions(_exchangeName, false);
 
         async Task<HttpResult<SharedSpotSymbol[]>> ISpotSymbolRestClient.GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
