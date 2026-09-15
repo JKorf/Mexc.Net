7c69e51fbea528025a6e29e2292f40bf2ac11e85
Jkorf <jankorf91@gmail.com>
2026-09-15 15:11:51 +0200

    wip

--- Mexc.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Mexc.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -29,30 +29,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             IConfiguration configuration)
         {
-            var options = new MexcOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-
-            try
-            {
-                configuration.Bind(options);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new InvalidOperationException("Invalid configuration provided", ex);
-            }
-
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            var restEnvName = options.Rest.Environment?.Name ?? options.Environment?.Name ?? MexcEnvironment.Live.Name;
-            var socketEnvName = options.Socket.Environment?.Name ?? options.Environment?.Name ?? MexcEnvironment.Live.Name;
-            options.Rest.Environment = MexcEnvironment.GetEnvironmentByName(restEnvName) ?? options.Rest.Environment!;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = MexcEnvironment.GetEnvironmentByName(socketEnvName) ?? options.Socket.Environment!;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
-
+            var options = MexcOptions.CreateFromConfiguration(configuration);
             services.AddSingleton(Options.Options.Create(options.Rest));
             services.AddSingleton(Options.Options.Create(options.Socket));
             services.AddSingleton(Options.Options.Create(options));
@@ -70,19 +47,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             Action<MexcOptions>? optionsDelegate = null)
         {
-            var options = new MexcOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-            optionsDelegate?.Invoke(options);
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            options.Rest.Environment = options.Rest.Environment ?? options.Environment ?? MexcEnvironment.Live;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = options.Socket.Environment ?? options.Environment ?? MexcEnvironment.Live;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
-
+            var options = MexcOptions.Create(optionsDelegate);
             services.AddSingleton(Options.Options.Create(options.Rest));
             services.AddSingleton(Options.Options.Create(options.Socket));
             services.AddSingleton(Options.Options.Create(options));
